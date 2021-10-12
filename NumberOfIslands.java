5a988ce4ed228b4744f57f82455a541b736df869
June Hong <June@skyglobal.com>
2021-10-11 17:42:27 -0700

    Create NumberOfIslands.java

---
+++ NumberOfIslands.java
@@ -0,0 +1,30 @@
+public class NumberOfIslands {
+    public static void main(String[] args) {
+        System.out.println("Hi!!!");
+    }
+}
+
+/**
+ * Given an m x n 2D binary grid which represents a map of '1's (lad) and '0's (water), 
+ * return the number of islands.
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ * You may assume all four edges of the grid are all surrouded by water.
+ * 
+ * Example 1:
+ * Input: grid = [
+ * ["1","1","1","1","0"],
+ * ["1","1","0","1","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ * 
+ * Example 2:
+ * Input: grid = [
+ * ["1","1","0","0","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","1","0","0"],
+ * ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ */
